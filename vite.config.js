6c6dc6b6ab7abc936891f68b4e2ac9e809b47247
SAKINA AZAM <205117adp@gmail.com>
2023-06-14 12:46:08 +0500

    change the base

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/images-Display/',
+  // base: '/images-Display/',
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
